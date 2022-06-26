75e69b61d97f48b5ed74bc811f4760f9efcc0466
apiwitdev <apiwit.pks@gmail.com>
2022-06-26 08:17:32 +0700

    Add top three coins tap

--- app/src/main/java/com/example/coinrank/ui/MainActivity.kt
+++ app/src/main/java/com/example/coinrank/ui/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.coinrank.ui
 
+import android.net.Uri
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
@@ -12,14 +15,15 @@ import com.example.coinrank.databinding.ActivityMainBinding
 import com.example.coinrank.model.CoinData
 import com.example.coinrank.repositories.LoadData
 import com.example.coinrank.viewmodel.CoinListViewModel
+import com.github.twocoffeesoneteam.glidetovectoryou.GlideToVectorYou
+import com.squareup.picasso.Picasso
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
 
-    private val limit : Int = 100
-    private var offset : Int = 0
+    private val limit : Int = 25
     private lateinit var binding:ActivityMainBinding
 
     private val loader = LoadData()
@@ -34,6 +38,8 @@ class MainActivity : AppCompatActivity() {
         binding = DataBindingUtil
             .setContentView(this,R.layout.activity_main)
 
+        binding.main = this
+
         binding.lifecycleOwner = this
 
         load()
@@ -45,7 +51,7 @@ class MainActivity : AppCompatActivity() {
                 if (layoutManager.findLastCompletelyVisibleItemPosition() == viewModel.name.size - 1
                     && viewModel.name[viewModel.name.size - 1] != "") {
                     viewModel.addEndPosition()
-                    offset += limit
+                    viewModel.offset += limit
                     load()
                 }
             }
@@ -53,7 +59,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun load() {
-        val res = loader.service.getCoins( R.string.apiKey.toString() , offset , limit )
+        val res = loader.service.getCoins( R.string.apiKey.toString() , viewModel.offset , limit )
         res.enqueue(object : Callback<CoinData> {
             override fun onResponse(call: Call<CoinData>, response: Response<CoinData>) {
                 val coins : CoinData
@@ -61,7 +67,11 @@ class MainActivity : AppCompatActivity() {
                     coins = response.body()!!
                     viewModel.removeEndPosition()
                     viewModel.setItem(coins, limit)
-                    if(offset == 0) {
+                    if(viewModel.offset == 0) {
+                        binding.viewModel = viewModel
+                        setTopThreeCoinIcon(binding.icon1 , viewModel.top3Icon[0])
+                        setTopThreeCoinIcon(binding.icon2 , viewModel.top3Icon[1])
+                        setTopThreeCoinIcon(binding.icon3 , viewModel.top3Icon[2])
                         binding.adapter = CoinListAdapter(viewModel)
                     }
                     (binding.adapter as CoinListAdapter).notifyDataSetChanged()
@@ -73,5 +83,20 @@ class MainActivity : AppCompatActivity() {
                 Toast.makeText(baseContext,"Fail Loading",Toast.LENGTH_LONG).show()
             }
         })
+    }
+
+    fun setTopThreeCoinIcon(view : View , iconUrl : String) {
+        if (iconUrl.contains(".svg")) {
+            GlideToVectorYou.init().with(view.context)
+                .load(Uri.parse(iconUrl), view as ImageView)
+        }
+        else {
+            Picasso.get().load(iconUrl).into(view as ImageView)
+        }
+    }
+
+    fun floatingButtonClick() {
+        binding.RV.smoothScrollToPosition(0)
+        Toast.makeText(baseContext,"go to top",Toast.LENGTH_SHORT).show()
     }
 }
--- app/src/main/java/com/example/coinrank/viewmodel/CoinListViewModel.kt
+++ app/src/main/java/com/example/coinrank/viewmodel/CoinListViewModel.kt
@@ -6,6 +6,15 @@ import java.text.DecimalFormat
 
 class CoinListViewModel : ViewModel() {
 
+    var offset : Int = 0
+
+    var top3Icon = mutableListOf<String>()
+    var top3name = mutableListOf<String>()
+    var top3symbol : MutableList<String> = mutableListOf()
+    var top3price : MutableList<String> = mutableListOf()
+    var top3change : MutableList<String> = mutableListOf()
+    var top3colorOfChangeValue : MutableList<Boolean> = mutableListOf()
+
     var iconUrl : MutableList<String> = mutableListOf()
     var name : MutableList<String> = mutableListOf()
     var symbol : MutableList<String> = mutableListOf()
@@ -16,27 +25,49 @@ class CoinListViewModel : ViewModel() {
     var position : Int = 0
 
     fun setItem(coins : CoinData , limit : Int) {
+        var initial : Int
+        if (offset == 0) {
+            initial = 3
+        }
+        else {
+            initial = 0
+        }
 
-        var format = DecimalFormat()
+        val format = DecimalFormat()
         format.applyPattern("#.###")
 
         for (position in 0 until limit) {
             val item = coins.data.coins[position]
-            iconUrl.add( item.iconUrl )
-            name.add( item.name )
-            symbol.add( item.symbol )
-
             var formatPrice : String = format.format(item.price)
+            if (position < initial) {
+                top3Icon.add( item.iconUrl )
+                top3name.add( item.name )
+                top3symbol.add( item.symbol )
 
-            price.add( "$"+formatPrice )
-
-            if (item.change >= 0) {
-                colorOfChangeValue.add(true)
+                top3price.add( "$"+formatPrice )
+                if (item.change >= 0) {
+                    top3colorOfChangeValue.add(true)
+                }
+                else {
+                    top3colorOfChangeValue.add(false)
+                }
+                top3change.add( item.change.toString() )
             }
             else {
-                colorOfChangeValue.add(false)
+                iconUrl.add( item.iconUrl )
+                name.add( item.name )
+                symbol.add( item.symbol )
+
+                price.add( "$"+formatPrice )
+
+                if (item.change >= 0) {
+                    colorOfChangeValue.add(true)
+                }
+                else {
+                    colorOfChangeValue.add(false)
+                }
+                change.add( item.change.toString() )
             }
-            change.add( item.change.toString() )
         }
     }
     fun addEndPosition() {
